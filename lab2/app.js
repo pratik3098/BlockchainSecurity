8e962b57633f2322a8bc2fb707a5a9f7f4526e85
Pratik Patil <101284380@georgebrown.ca>
2019-11-27 19:52:27 -0500

    ganache server added

--- lab2/app.js
+++ lab2/app.js
@@ -1,11 +1,11 @@
 const ethers = require('ethers')
-const ganache = require("ganache-cli")
-let provider = new ethers.providers.Web3Provider(ganache.provider())
-//let provider=new ethers.providers.JsonRpcProvider()
+const ganache = require('ganache-cli')
+const server = ganache.server()
+let port = 8090
+let provider=new ethers.providers.JsonRpcProvider('http://localhost:'+port)
 
 let privateKey= '0x5aebccecefb08a00a7c74d68a17b973d1bb5896e1194c6e949fe144747677c8d'
 let wallet= new ethers.Wallet(privateKey, provider)
-
 
 
 wallet.sendTransaction({
@@ -14,7 +14,10 @@ wallet.sendTransaction({
 }).then(tx => {
    console.log(tx)
 }).catch(err=>{
-   console.error('Error: ',err )
+   console.error('Error: ', err.message )
 }) 
 
+server.listen(port,()=>{
+   console.log('Server listen port : '+ port)
+})
 
