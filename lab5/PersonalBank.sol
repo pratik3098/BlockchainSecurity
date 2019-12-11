86fe9234ac3c67728d10129194d4cae2e60fc264
pratik3098 <pratik3098@gmail.com>
2019-12-11 13:34:06 -0500

    lab5 added

--- lab5/PersonalBank.sol
+++ lab5/PersonalBank.sol
@@ -2,17 +2,17 @@ pragma solidity ^0.5.0;
 
 contract PersonalBank {
     address owner;
+    address current_payable;
     
     constructor() public payable {
         owner = msg.sender;
+        current_payable = address(0);
     }
     
     function() external payable {
     }
     
-    function cashCheque(address payable to, uint256 amount,
-                        bytes32 r, bytes32 s, uint8 v)
-                public {
+    function cashCheque(address payable to, uint256 amount,  bytes32 r, bytes32 s, uint8 v) public only_owner() attack_safety(to){
         bytes32 messageHash = keccak256(abi.encodePacked(to, amount));
         
         bytes32 messageHash2 = keccak256(abi.encodePacked(
@@ -22,5 +22,18 @@ contract PersonalBank {
         require(ecrecover(messageHash2, v, r, s) == owner, "bad signature");
         
         to.transfer(amount);
+        
+        current_payable = address(0);
+    }
+    
+     modifier attack_safety(address payable_to){
+        require(payable_to != current_payable ,"Error: Multiple cheque cashing");
+        current_payable=payable_to;
+        _;
+    }
+    
+    modifier only_owner(){
+       require(owner==msg.sender,"Error: Invalid transcation");
+         _;
     }
 }
